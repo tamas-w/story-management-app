def05d596592574c71160c76a18f53d0d635bc92
lehel_laszlo <lehel@cozycozy.com>
2022-09-30 10:10:30 +0200

    feat - DB connection created

---
+++ Backend/data_source.ts
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import { User } from "./src/entity/user.entity";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  synchronize: true,
+  entities: [User],
+});
